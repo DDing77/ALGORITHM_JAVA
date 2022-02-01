b56cc7bcdf76d96bf50bc75dc606d1c4663fa4a4
DDing77 <smg76262585@gmail.com>
2022-02-02 00:40:13 +0900

    first commit

---
+++ BOJ_1000.java
@@ -0,0 +1,12 @@
+package BOJ;
+import java.io.*;
+import java.util.*;
+public class BOJ_1000 {
+	public static void main(String[] args) throws IOException {
+BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.println(str);
+	}
+	
+
+}
---
+++ BOJ_11651.java
@@ -0,0 +1,9 @@
+package BOJ;
+
+public class BOJ_11651 {
+
+	public BOJ_11651() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
