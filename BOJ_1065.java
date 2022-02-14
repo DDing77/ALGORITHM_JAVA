b699bf0cd9188971c7db39f81d8e67b9ff22d16a
DDing77 <smg76262585@gmail.com>
2022-02-15 01:11:54 +0900

    BOJ_1065

---
+++ BOJ_1065.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1065 {
+	static int X;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		X = Integer.parseInt(br.readLine());
+		
+		if (X < 100) {
+			System.out.println(X);
+		}
+		else {
+			int cnt = 99;
+			for (int i = 100; i <= X; i++) {
+				int a = i / 100;
+				int b = (i / 10) % 10;
+				int c = i% 10;
+				if ((a - b) == (b - c)) cnt++;
+			}
+			System.out.println(cnt);
+		}
+	}
+}
