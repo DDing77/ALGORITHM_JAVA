36d99dcbeafc106ff4f0b869243bb4f895ed858b
DDing77 <smg76262585@gmail.com>
2022-02-23 20:12:42 +0900

    11728

--- BOJ_1065.java
+++ BOJ_1065.java
@@ -4,23 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// author : DDing
+// solution : 브루트포스
+
 public class BOJ_1065 {
 	static int X;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		X = Integer.parseInt(br.readLine());
-		
+
 		if (X < 100) {
 			System.out.println(X);
-		}
-		else {
+		} else {
 			int cnt = 99;
 			for (int i = 100; i <= X; i++) {
 				int a = i / 100;
 				int b = (i / 10) % 10;
-				int c = i% 10;
-				if ((a - b) == (b - c)) cnt++;
+				int c = i % 10;
+				if ((a - b) == (b - c))
+					cnt++;
 			}
 			System.out.println(cnt);
 		}
---
+++ BOJ_11728.java
@@ -0,0 +1,33 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ_11728 {
+	static int N, M;
+	static ArrayList<Integer> A; 
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = new ArrayList<>();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			A.add(Integer.parseInt(st.nextToken()));
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			A.add(Integer.parseInt(st.nextToken()));
+		}
+		Collections.sort(A);
+		for(int x : A) sb.append(x).append(" ");
+		System.out.print(sb);
+	}
+}
