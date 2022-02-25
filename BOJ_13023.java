802e9b249f886bd1e8e45e8550389197747b2883
DDing77 <smg76262585@gmail.com>
2022-02-26 05:05:13 +0900

    BOJ_18243

--- BOJ_13023.java
+++ BOJ_13023.java
@@ -10,38 +10,41 @@ public class BOJ_13023 {
 	static int N, M;
 	static ArrayList<Integer>[] list;
 	static boolean isVisit[];
+
 	static void DFS(int start, int cnt) {
-		if(cnt == 4) {
+		if (cnt == 4) {
 			System.out.println(1);
 			System.exit(0);
 		}
 		isVisit[start] = true;
-		for(int next : list[start]) {
-			if(!isVisit[next]) {
+		for (int next : list[start]) {
+			if (!isVisit[next]) {
 				isVisit[next] = true;
-				DFS(next, cnt+1);
+				DFS(next, cnt + 1);
 				isVisit[next] = false;
 			}
 		}
 	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		list = new ArrayList[N];
-		for(int i=0; i<N; i++) list[i] = new ArrayList<>();
-		for(int i=0; i<M; i++) {
+		for (int i = 0; i < N; i++)
+			list[i] = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			list[start].add(end);
 			list[end].add(start);
 		}
-		
-		for(int i=0; i<N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			isVisit = new boolean[N];
 			DFS(i, 0);
 		}
---
+++ BOJ_18243.java
@@ -0,0 +1,58 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_18243 {
+	static int N, K;
+	static long[][] map;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		map = new long[N+1][N+1];
+		
+		for(int i=0; i<=N; i++) {
+			for(int j=0; j<=N; j++) {
+				if(i == j) {
+					map[i][j] = 0;
+					continue;
+				}
+				map[i][j] = Integer.MAX_VALUE;
+			}
+			
+		}
+		
+		for(int i=0; i<K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			map[start][end] = 1;
+			map[end][start] = 1;
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+				}
+			}
+		}
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				if(map[i][j] >= 7 || map[i][j] == Integer.MAX_VALUE) {
+					System.out.println("Big World!");
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println("Small World!");
+	}
+}
